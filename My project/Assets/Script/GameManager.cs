a7de0523ae4e9f7d290daa068f8de3d04a07408f
MSI\GA3 <h1202416002@hiratagakuen.onmicrosoft.com>
2026-04-13 13:43:56 +0900

    タイマー追加＆ポーズ画面追加

---
+++ My project/Assets/Script/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    //ポーズ画面
+    public GameObject Pause;
+    private bool isVisible = false;
+    //タイマー
+    public Text TimeText;
+    private float TimeCount;
+    void Start()
+    {
+        TimeCount = 0f;
+    }
+
+    void Update()
+    {
+        //タイマー
+        TimeCount += Time.deltaTime;
+        TimeText.text = TimeCount.ToString("F1");
+
+        //ポーズ画面
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            Time.timeScale = 0;
+            isVisible = !isVisible;
+            Pause.SetActive(isVisible);
+        }
+        if (!isVisible)
+        {
+            Time.timeScale = 1;
+        }
+
+    }
+}
