c2576678dc7b5b6c5d40fe4d6ca460344297d0dc
MSI\GA3 <h1202416002@hiratagakuen.onmicrosoft.com>
2026-04-15 15:30:53 +0900

    ステージ３仮作成

--- My project/Assets/Script/Switch.cs
+++ My project/Assets/Script/Switch.cs
@@ -6,7 +6,8 @@ public class Switch : MonoBehaviour
 {
     private int SwitchCount; //スイッチを踏んでいる数を確かめるため
 
-    public GameObject SwitchWall; //消える壁
+    public GameObject[] DisappearWalls; //消える壁の配列
+    public GameObject[] AppearingWalls; //出現する壁の配列
     void Start()
     {
         SwitchCount = 0;
@@ -15,7 +16,14 @@ public class Switch : MonoBehaviour
     {
         if (other.CompareTag("Ball"))
         {
-            SwitchWall.SetActive(false);
+            foreach (GameObject DWall in DisappearWalls) //スイッチを踏んでいる間消える壁
+            {
+                DWall.SetActive(false);
+            }
+            foreach (GameObject AWall in AppearingWalls) //スイッチを踏んでいる間出現する壁
+            {
+                AWall.SetActive(true);
+            }
 
         }
     }
@@ -23,10 +31,15 @@ public class Switch : MonoBehaviour
     {
         if (other.CompareTag("Ball"))
         {
-            SwitchWall.SetActive(true);
+            foreach (GameObject DWall in DisappearWalls) //スイッチを踏んでいる間消える壁
+            {
+                DWall.SetActive(true);
+            }
+            foreach (GameObject AWall in AppearingWalls) //スイッチを踏んでいる間出現する壁
+            {
+                AWall.SetActive(false);
+            }
 
         }
     }
-
-
 }
