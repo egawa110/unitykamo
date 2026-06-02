5beed9efc49271fedcb0544db9f81d7f0fea472e
MSI\GA3 <h1202416002@hiratagakuen.onmicrosoft.com>
2026-06-02 16:31:27 +0900

    ステージ２作り途中

--- My project (1)/Assets/Script/MoveBridge.cs
+++ My project (1)/Assets/Script/MoveBridge.cs
@@ -3,23 +3,38 @@ using UnityEngine;
 public class MoveBridge : MonoBehaviour
 {
     const float speed = 0.01f;
-    const float posx = 0.3f;
+    public float move = 0;
+    private float posx;
+    private int count;
+    const int maxcount = 2; //カウントの上限
     private Vector3 pos;
     private Vector3 nextpos;
 
-    public MoveGround mg;
     void Start()
     {
         pos = transform.position;
-        nextpos.x = pos.x + posx;
+        posx = pos.x; //最初の位置を保存
+        nextpos.x = pos.x + move;
+        count = 0; //posが指定の場所に到着したら+する
     }
 
     void Update()
     {
-        if(nextpos.x <= pos.x)
+        if(nextpos.x >= pos.x && count == 0)// 指定の位置に向かう
         {
             pos.x += speed;
-
+            if(nextpos.x <= pos.x)
+            count = 1;
+        }
+        else if (count != maxcount) //指定の位置まで行けたら折り返し
+        {
+            pos.x -= speed;
+            if (pos.x <= posx)
+                count = 2;
+        }
+        if (count >= maxcount) //カウントが上限まで行ったら初期化
+        {
+            count = 0;
         }
 
         transform.position = pos;
--- My project (1)/Assets/Script/Player.cs
+++ My project (1)/Assets/Script/Player.cs
@@ -90,7 +90,6 @@ public class Player : MonoBehaviour
             abyssflag = false;
 
         }
-
     }
 
     void Update()
